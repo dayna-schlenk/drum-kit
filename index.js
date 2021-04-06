b245330ad8c68251f4fcaf3aa982d937bd2db185
dayna-schlenk <dayna.schlenker@gmail.com>
2021-04-06 18:26:30 -0500

    Button text color changes to white when clicked

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// document.querySelectorAll("button").addEventListener("click", function() {
-//     alert("I got clicked!");
-// })
-
 const drumBtns = document.querySelectorAll(".drum");
 
 for (let i = 0; i < drumBtns.length; i++) {
     drumBtns[i].addEventListener("click", function() {
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
+        this.style.color = "white";
     })
 }
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
