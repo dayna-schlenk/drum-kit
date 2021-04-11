5c0451aff404a4d2e99de473690098b417e07df2
dayna-schlenk <dayna.schlenker@gmail.com>
2021-04-11 14:49:14 -0500

    Add animation to button and key presses

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ for (let i = 0; i < drumBtns.length; i++) {
     drumBtns[i].addEventListener("click", function() {
         var buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
     })
 }
 
 // Detecting keyboard press
 document.addEventListener("keydown", function(event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
 })
 
 function makeSound(key) {
@@ -52,4 +54,12 @@ function makeSound(key) {
         
             default: console.log(buttonInnerHTML);
         }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
